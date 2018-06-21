4430b9c69f5acc5f491850a1a5ee528dddbf8de5
Uche Jude <iamuchejude@gmail.com>
2018-06-21 17:59:12 +0100

    New Update

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import rides from '../controllers';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.status(400).json({
-    message: 'Error Message',
+    status: 'error',
+    message: 'Not found',
   });
 });
 
+router.get('/rides', rides.getAllRideOffers);
+router.get('/rides/:id', rides.getOneRideOffer);
+
+
 module.exports = router;
+export { router as default };
